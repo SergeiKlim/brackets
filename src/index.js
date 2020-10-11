462b196099c58a19cc9a057fb841fe0b639d1356
SergeiKlim <70857184+SergeiKlim@users.noreply.github.com>
2020-10-11 19:28:26 +0300

    init: start JS:Brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let l = 0;
+  while(str.length != l) {
+    l = str.length;
+  for(let i = 0; i < bracketsConfig.length; i++) {
+    let a = bracketsConfig[i][0] + bracketsConfig[i][1];
+    str = str.replace(a,"");
+  }
+}
+
+return str.length == 0;  
 }
